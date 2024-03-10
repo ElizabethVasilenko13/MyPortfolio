2f1fff6ba0fcb839fed65bb948ce811462e71473
Yelyzaveta <liza.vasilenko13@gmail.com>
2024-03-11 00:09:38 +0100

    feat: add basic main page

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,13 +1,23 @@
-import { Component } from '@angular/core';
+import { Component, Renderer2, inject } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, RouterLink],
   templateUrl: './home.component.html',
-  styleUrls: ['./home.component.scss']
+  styleUrls: ['./home.component.scss'],
 })
 export class HomeComponent {
+  private renderer = inject(Renderer2);
 
+  downloadFile(): void {
+    const link = this.renderer.createElement('a');
+    link.setAttribute('target', '_blank');
+    link.setAttribute('href', '../../assets/CV_Yelyzaveta_Vasylenko_Frontend.pdf');
+    link.setAttribute('download', 'CV_Yelyzaveta_Vasylenko_Frontend.pdf');
+    link.click();
+    link.remove();
+  }
 }
